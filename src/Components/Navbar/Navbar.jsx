3fb9f240d0d3d8bc118bba19cdd38917a60dc45e
Michael Chege <mikechege770@gmail.com>
2024-09-28 21:00:00 +0300

    djad

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -23,7 +23,11 @@ const Navbar = () => {
   };
 
   return (
-    <nav className={`w-full fixed top-0 left-0 z-10 flex items-center justify-between p-4 ${sticky ? "bg-[#212ea0] transition duration-500" : "bg-transparent"}`}>
+    <nav
+      className={`w-full fixed top-0 left-0 z-10 flex items-center justify-between p-4 ${
+        sticky ? "bg-[#212ea0] transition duration-500" : "bg-transparent"
+      }`}
+    >
       <img src={logo} alt="Logo" className="w-[45px] md:w-[38px]" />
       <img
         src={menu_icon}
@@ -31,11 +35,13 @@ const Navbar = () => {
         className="md:hidden w-[26px] cursor-pointer z-20"
         onClick={toggleMenu}
       />
-      <ul className={`flex md:flex-row flex-col md:items-center fixed md:static bg-[#212ea0] md:bg-transparent h-full md:w-auto transition-all duration-300 ease-in-out
+      <ul
+        className={`flex md:flex-row flex-col md:items-center fixed md:static bg-[#212ea0] md:bg-transparent h-full md:w-auto transition-all duration-300 ease-in-out
         ${mobileMenu ? "w-[50vw] right-0 top-0" : "w-0 right-[-100vw]"} 
-        md:top-0 md:right-auto md:p-0 p-4 z-10`}>
+        md:top-0 md:right-auto md:p-0 p-4 z-10`}
+      >
         {/* Added margin-top to separate first menu item from the icon */}
-        <li className="list-none mx-6 text-white text-[15px] mt-12 md:mt-0">  
+        <li className="list-none mx-6 text-white text-[15px] mt-12 md:mt-0">
           <Link to="hero" smooth={true} offset={0} duration={500}>
             Home
           </Link>
@@ -51,11 +57,18 @@ const Navbar = () => {
             About Us
           </Link>
         </li>
-        <li className="list-none mx-6 text-white text-[15px] mt-8 md:mt-0">
-          <Link to="contact" smooth={true} offset={-210} duration={500} className="btn text-sm md:text-[15px]">
-            Contact Us
-          </Link>
-        </li>
+        <li className="list-none mx-4 text-white text-[11px] md:text-[15px] mt-8 md:mt-0">
+  <Link 
+    to="contact" 
+    smooth={true} 
+    offset={-210} 
+    duration={500} 
+    className="btn text-[11px] !important  md:text-[15px]"  // Setting a smaller text size
+  >
+    Contact Us
+  </Link>
+</li>
+      
       </ul>
     </nav>
   );
