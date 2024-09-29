361f0c5119ad2220584fe3d024382f658cf4c7d8
Michael Chege <mikechege770@gmail.com>
2024-09-29 08:42:30 +0300

    fkkffs

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <div className="mx-auto flex items-center justify-between border-t border-gray-500 py-4 max-w-7xl text-sm">
       <p>© 2024 Upperhill College. All rights reserved.</p>
-      <ul className="flex space-x-5">
+      <ul className=" ml-6  flex space-x-5">
         <li className="list-none cursor-pointer hover:underline">Terms of Services</li>
         <li className="list-none cursor-pointer hover:underline">Privacy Policy</li>
       </ul>
